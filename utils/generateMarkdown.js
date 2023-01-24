a780e6a025dfe26e6d574ef8d17f1564d8093d14
Hollusam <oluyomisamuels@gmail.com>
2023-01-24 10:14:21 +0000

    generate markdown JS file updated

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,54 @@
+// function to select license batch
+function licenseBadge(data) {
+  const license = data.license[0];
+  let licenseUrl = ""
+  if (license) {
+    licenseUrl = `![Github license](http://img.shields.io/badge/license-${data.license}-green.svg)`;
+  }
+  return licenseUrl
+};
+
+
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ![Github license](http://img.shields.io/badge/license-${data.license}-green.svg)
 
+## Description
+${data.description}
+
+## Table of Contents
+
+* [Installation](#install)
+* [Usage](#usage)
+* [Screenshot](#screenshot)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+${data.install}
+
+## Usage
+${data.usage}
+
+## Screenshot
+![Screenshot](${data.screenshot})
+
+## License
+${licenseBadge(data)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.test}
+
+## Questions
+Feel free to contact at ${data.email} to ask any questions about this project. To view other projects by me; please visit https://github.com/${data.github}
 `;
 }
 
